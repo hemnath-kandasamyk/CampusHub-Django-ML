88ffe1d67a613d5e891932aaf6c8ecbd6abe1647
Hemnath KK <hemnathkandasamyk@gmail.com>
2026-08-20 13:56:49 +0530

    feat: implement Student class

--- python_oop/01_classes_objects.py
+++ python_oop/01_classes_objects.py
@@ -1,14 +1,24 @@
 class Student:
 
-    def __init__(self, name, age):
+    def __init__(self, name, email, department, year):
         self.name = name
-        self.age = age
+        self.email = email
+        self.department = department
+        self.year = year
 
-    def introduce(self):
-        print(f"My name is {self.name}")
-        print(f"I am {self.age} years old")
+    def display_profile(self):
+        print("\n--- Student Profile ---")
+        print("Name       :", self.name)
+        print("Email      :", self.email)
+        print("Department :", self.department)
+        print("Year       :", self.year)
 
 
-student = Student("Hemnath", 21)
+student = Student(
+    "Hemnath",
+    "hemnath@example.com",
+    "AI & DS",
+    3
+)
 
-student.introduce()
+student.display_profile()
